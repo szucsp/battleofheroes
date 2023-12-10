4786e820b9f2edd92156df14df7698901ae74135
Peter Szucs <Peter.Szucs@quest.com>
2023-12-10 20:24:28 +0000

    Created hero base class

---
+++ Heroes/HeroBase.cs
@@ -0,0 +1,36 @@
+﻿namespace Heroes
+{
+    public abstract class HeroBase
+    {
+        private const double diedlimit = 0.25;
+
+        public int Id { get; set; }
+        public int Vitality { get; protected set; }
+
+        public int MaxVitality { get; protected set; }
+
+        public HeroState State { get; protected set; }
+
+        public void SetState(HeroState state)
+        {
+            this.State = state;
+        }
+
+        public void SetVitality(HeroVitalityChange vitalityChange)
+        {
+            int value = 0;
+
+            if (vitalityChange == HeroVitalityChange.Up)
+                value = 10;
+            if (vitalityChange == HeroVitalityChange.Down)
+                value = (Vitality / 2) * -1;
+
+            this.Vitality = ((this.Vitality + value) > MaxVitality) ? this.MaxVitality : this.Vitality + value;
+
+            if (this.Vitality < this.MaxVitality * diedlimit)
+            {
+                SetState(HeroState.Died);
+            }
+        }
+    }
+}
