233d60e5d62845c54a3b7779ad1c7cac1a2bc939
Peter Szucs <Peter.Szucs@quest.com>
2023-12-10 20:34:11 +0000

    Created generator class for heroes

---
+++ GeneratorForHeroes/HeroGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Heroes;
+
+namespace GeneratorForHeroes
+{
+    public class HeroGenerator
+    {
+        public List<HeroBase> GetHeros(int n)
+        {
+            List<Type> heroTypes = GetHeroTypes();
+            List<HeroBase> heros = new List<HeroBase>();
+            var random = new Random();
+
+            for (int i = 1; i <= n; i++)
+            {
+                var heroType = heroTypes[random.Next(heroTypes.Count)];
+
+                var newInstance = (HeroBase)Activator.CreateInstance(heroType);
+
+                newInstance.Id = i;
+
+                heros.Add(newInstance);
+            }
+            return heros;
+        }
+
+        private List<Type> GetHeroTypes()
+        {
+            List<Type> heroTypes = new List<Type>();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var assembly in assemblies)
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.IsSubclassOf(typeof(HeroBase)))
+                        heroTypes.Add(type);
+                }
+            }
+            return heroTypes;
+        }
+    }
+}
