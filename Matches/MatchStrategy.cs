63dc4ec6ea447e967bd75eccf6fe3bb4367ab508
P Sz <szucsp1981@gmail.com>
2023-12-10 21:49:22 +0000

    Merged in feature/commentsForTheClasses (pull request #6)
    Comments, and fixing some name of variable
    
    * Comments, and fixing some name of variable

--- Matches/MatchStrategy.cs
+++ Matches/MatchStrategy.cs
@@ -2,6 +2,9 @@
 
 namespace Matches
 {
+    /// <summary>
+    /// This is an abstract class for strategies
+    /// </summary>
     public abstract class MatchStrategy
     {
         public abstract HeroBase DoMatch(HeroBase assailant, HeroBase defender);
