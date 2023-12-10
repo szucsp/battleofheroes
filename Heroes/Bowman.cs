1888b3993c68f8718e137371cebeca9a5551cb3b
Peter Szucs <Peter.Szucs@quest.com>
2023-12-10 20:25:54 +0000

    Created "bowman" hero class based on HeroBase

---
+++ Heroes/Bowman.cs
@@ -0,0 +1,12 @@
+﻿namespace Heroes
+{
+    public sealed class Bowman : HeroBase
+    {
+        public Bowman()
+        {
+            MaxVitality = 100;
+            Vitality = MaxVitality;
+            State = HeroState.Live;
+        }
+    }
+}
