2644e48e5c331bc7542a5b24640dea1d8be09886
Peter Szucs <Peter.Szucs@quest.com>
2023-12-10 20:45:35 +0000

    created strategy class for swordsman and rider

---
+++ Matches/MatchBetsweenSwordsmanAndRider.cs
@@ -0,0 +1,23 @@
+﻿using Heroes;
+
+namespace Matches
+{
+    [Match("Swordsman", "Rider")]
+    public class MatchBetweenSwordsManAndRiderStrategy : MatchStrategy
+    {
+        public override HeroBase DoMatch(HeroBase assailant, HeroBase defender)
+        {
+            if (assailant.GetType() == typeof(Swordsman))
+            {
+                return null;
+            }
+
+            if (assailant.GetType() == typeof(Rider))
+            {
+                return assailant;
+            }
+
+            return null;
+        }
+    }
+}
